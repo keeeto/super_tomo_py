eb90a6ef40adbe47634ea0193e118b0a77d76bdc
mts87985 <mts87985@dev03.pearl.scd.stfc.ac.uk>
2020-07-16 09:38:05 +0100

    update cnn_reconstruct to hongyangs best model

--- models/cnn_reconstruct/models.py
+++ models/cnn_reconstruct/models.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D
 from tensorflow.keras.layers import UpSampling2D, Flatten, Reshape 
-from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import Conv2DTranspose, Dropout
 from tensorflow.keras.models import Model
 
 def reconstruct_cnn(ntr, npr):
@@ -14,45 +14,26 @@ def reconstruct_cnn(ntr, npr):
        A tensorflow model
     '''
 
-    inputs = Input((ntr, npr, 1))
-
-    convin1 = Conv2D(
-        filters = 64, kernel_size = (3,3), strides = 2,
-        padding = 'same', activation = 'relu', kernel_initializer = 'he_normal')(inputs)
-    convin2 = Conv2D(
-        filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(convin1)
-    convin3 = Conv2D(
-        filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(convin2)
-
-    flatten = Flatten()(convin3)
-    dense1 = Dense(ntr*ntr, activation = 'relu')(flatten)
-    dense2 = Dense(ntr*ntr, activation = 'relu')(dense1)
-    reshape = Reshape((ntr, ntr, 1))(dense2)
-
-
-    conv1 = Conv2D(
-        filters = 64, kernel_size = (7,7), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(reshape)
-    conv2 = Conv2D(
-        filters = 64, kernel_size = (5,5), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(conv1)
-    conv3 = Conv2D(
-        filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(conv2)
-
-    deconv1 = Conv2DTranspose(
-        filters = 64, kernel_size = (7,7), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(conv3)
-    deconv2 = Conv2DTranspose(
-        filters = 64, kernel_size = (5,5), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(deconv1)
-    deconv3 = Conv2DTranspose(
-        filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu',
-        kernel_initializer = 'he_normal')(deconv2)
-
-    outputs = Conv2D(1, 3, padding='same')(deconv3)
+    inputs = Input(shape = (ntr,npr,1))
+    conv = Conv2D(filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu')(inputs)
+    conv = Conv2D(filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu')(conv)
+    conv = Conv2D(filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu')(conv)
+    conv = Conv2D(filters = 64, kernel_size = (3,3), strides = 2, padding = 'same', activation = 'relu')(conv)
+    flat = Flatten()(conv)
+    dense = Dense(1000, activation = 'relu')(flat)
+    drop = Dropout(0.1)(flat)
+    dense = Dense(1000, activation = 'relu')(drop)
+    drop = Dropout(0.1)(flat)
+    dense = Dense(1000, activation = 'relu')(drop)
+    drop = Dropout(0.1)(flat)
+    dense = Dense(1000, activation = 'relu')(drop)
+    drop = Dropout(0.1)(flat)
+    dense = Dense(ntr*ntr, activation = 'linear')(drop)
+    reshape = Reshape((ntr,ntr,1))(dense)
+    conv = Conv2D(filters = 128, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu')(reshape)
+    conv = Conv2D(filters = 128, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu')(conv)
+    conv = Conv2D(filters = 128, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu')(conv)
+    outputs = Conv2D(1, 3, padding='same')(conv)
 
     return Model(inputs, outputs)
     
